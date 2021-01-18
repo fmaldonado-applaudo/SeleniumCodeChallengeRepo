5faa875ad7d6c27d6753e91dae0ef9337faade0c
Fernando Maldonado <fmaldonado@applaudostudios.com>
2021-01-18 12:57:39 -0600

    Rest assured feedback 2

--- CodeChallengeSeleniumFernando/RestAssured/src/test/java/restasssured/RestAssuredChallenge.java
+++ CodeChallengeSeleniumFernando/RestAssured/src/test/java/restasssured/RestAssuredChallenge.java
@@ -1,36 +1,55 @@
 package restasssured;
 
 
+import endpoints.EndPoints;
+import io.restassured.response.Response;
 import restassuredmodel.Characters;
 import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
 
 import static io.restassured.RestAssured.*;
 
 public class RestAssuredChallenge {
     private int iCharId;
     private final String newLine = System.getProperty("line.separator");
+    EndPoints oEndpointRespone = new EndPoints();
 
     @Test(groups = "allChars")
     public void getAllCharacters() {
         System.out.println("Exercise 2: Get all characters");
-        Characters[] aCharacters = given().headers("Content-Type", ContentType.JSON, "Accept", ContentType.JSON).when().get("https://www.breakingbadapi.com/api/characters").as(Characters[].class);
-          //Arrays.stream(aCharacters).filter()
-        for (Characters oChar : aCharacters){
-            if (oChar.getName().equals("Walter White")) {
-                iCharId = oChar.getChar_id();
-            }
-            System.out.println("Character name: " + oChar.getName() + newLine + "Portrayed: " + oChar.getPortrayed() + newLine + "------------------------------------------------------");
-        }
 
+        Response epResponse = oEndpointRespone.doGetRequest("https://www.breakingbadapi.com/api/characters");
+        Characters[] aCharacters = epResponse.as(Characters[].class);
+
+        //Exercise 2
+        //ITERATE AND ASSIGN ID WHEN NAME IS WALTER WHITE
+        Arrays.stream(aCharacters).
+                forEach(oChar ->
+                {
+                    if (oChar.getName().equals("Walter White")) {
+                        iCharId = oChar.getChar_id();
+                    }
+                    System.out.println("Character name: " + oChar.getName() + newLine + "Portrayed: " + oChar.getPortrayed() + newLine + "------------------------------------------------------");
+                });
+
+        //Exercise 1
         System.out.println("Exercise 1: Get Walter White info");
         Assert.assertNotNull(iCharId);
-        aCharacters = given().headers("Content-Type", ContentType.JSON, "Accept", ContentType.JSON).when().get("https://www.breakingbadapi.com/api/characters/" + iCharId).as(Characters[].class);
-        for (Characters oChar : aCharacters){
-            Assert.assertEquals(oChar.getBirthday(),"09-07-1958");
-            System.out.println("Birthday: " + oChar.getBirthday());
-        }
+
+        epResponse = oEndpointRespone.doGetRequest("https://www.breakingbadapi.com/api/characters/" + iCharId);
+        aCharacters = epResponse.as(Characters[].class);
+
+        //Verify walter white birthday and then print it
+        Arrays.stream(aCharacters). forEach(
+                oChar -> {
+                    Assert.assertEquals(oChar.getBirthday(), "09-07-1958");
+                    System.out.println("Birthday: " + oChar.getBirthday());
+                }
+        );
+
     }
 
 
